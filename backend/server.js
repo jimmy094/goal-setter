4756482743bc96e48ad48dd6a33b2496d75654fc
jimmyalvarez <jimmyalvarezjr@gmail.com>
2022-02-16 15:57:04 -0500

    initial rest api for goals

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+
+const colors = require('colors')
+const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
+
+
+
+connectDB()
 
 const app = express();
 
 //middleware in order to use body data use express json and urlencoded
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+
 
 
 app.use('/api/goals', require('./routes/goalRoutes'))
