64abbd3c42cb32186475cb4df7f9503656094a48
jimmyalvarez <jimmyalvarezjr@gmail.com>
2022-05-19 12:20:15 -0400

    usercontroller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,97 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+const { MongoExpiredSessionError, MongoNetworkError } = require('mongodb')
+
+
+//@desc  register new user
+//@route POST /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+
+    const { name, email, password } = req.body 
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+     //check if user exists
+
+     const userExists = await User.findOne({email})
+
+     if(userExists) {
+         res.status(400)
+         throw new Error('User already exists')
+     }
+ 
+     //Hash password
+
+     const salt = await bcrypt.genSalt(10)
+
+     const hashedPassword = await bcrypt.hash(password, salt)
+
+     //Create user
+
+     const user = await User.create({
+         name,
+         email,
+         password: hashedPassword
+     })
+
+
+     if(user){
+         res.status(201).json({
+             _id: user.id,
+             name: user.name,
+             email: user.email
+         })
+     } else {
+         res.status(400)
+         throw new Error('Invalid user data')
+     }
+
+    
+})
+
+//@desc  authenticate user
+//@route POST /api/users/login
+//@access Public
+const loginUser = asyncHandler(async (req, res) => {
+
+    const user = await User.findOne({ email })
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        }) 
+        } else {
+            res.status(400)
+            throw MongoNetworkError('Invalid credentials')
+    }
+
+    
+    res.json({ message: 'Login User' })
+})
+
+//@desc  get user data
+//@route Get /api/users/me
+//@access Private
+const getMe = asyncHandler(async(req, res) => {
+
+    
+
+   
+
+    res.json({ message: 'User data display' })
+})
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe
+}
